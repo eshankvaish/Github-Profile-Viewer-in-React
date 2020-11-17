e5b2ee13cca651d1b269a69d9dd610ea4ec7ce22
Eshank Vaish <eshank.vaish@joshtechnologygroup.com>
2020-11-17 13:24:21 +0530

    [RA-1] Base Setup - Review Fixes - Added ESLINT and .eslint file - Changed the files according to rules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 /*Main App*/
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t , i18n} = useTranslation();  //t: for adding keys
-  i18n.changeLanguage(navigator.language || navigator.userLanguage);
+    const {i18n} = useTranslation();  //t: for adding keys
+    i18n.changeLanguage(navigator.language || navigator.userLanguage);
 
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss'; //Importing scss file
 import App from './App';
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 (function() {
-  ReactDOM.render(
-    <React.StrictMode>
-      {/* App wrapped inside I18 */}
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+    ReactDOM.render(
+        <React.StrictMode>
+            {/* App wrapped inside I18 */}
+            <I18nextProvider i18n={i18n}>
+                <App />
+            </I18nextProvider>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
 })();
