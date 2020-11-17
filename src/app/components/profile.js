1d8034a098f2e12dcea112626acff3875fd05a66
Eshank Vaish <eshank.vaish@joshtechnologygroup.com>
2020-11-17 11:56:09 +0530

    [RA-4] Own Profile Page - Own Profile Page native Code - Localization - Styling - Private Routes

--- src/app/components/profile.js
+++ src/app/components/profile.js
@@ -9,8 +9,39 @@ const Profile = (props) => {
         userData = "Other User Data";
     }
     return (
-        <section className="profile center-container">
-            Welcome to your Profile
+        <section className="profile-container center-container">
+            <h1 className="heading">{userData.name}{t("'s")} {t("Profile")}</h1>
+            <div className="profile">
+                <div className="profile__avatar">
+                    <img className="profile__avatar--img" src={userData.avatar} alt="User Profile Pic"/>
+                </div>
+                <ul className="profile__data">
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Username")}: </span>{userData.username}
+                    </li>
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Location")}: </span>{userData.location}
+                    </li>
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Followers")}: </span>{userData.followers_count}
+                    </li>
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Following")}: </span>{userData.following_count}
+                    </li>
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Bio")}: </span>{userData.bio}
+                    </li>
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Github Profile")}: </span><a href={userData.profile_link} aria-label="Profile Link">{userData.profile_link}</a>
+                    </li>
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Email")}: </span>{userData.email}
+                    </li>
+                    <li className="profile__data--item">
+                        <span className="profile__data--attribute">{t("Blog")}: </span>{userData.blog}
+                    </li>     
+                </ul>
+            </div>
             {/* To be Completed in RA-4 */}
         </section>
     );
