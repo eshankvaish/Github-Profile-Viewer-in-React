6754379f4ed0a238ce407c31cdb98ecb3445394f
Eshank Vaish <eshank.vaish@joshtechnologygroup.com>
2020-11-27 19:22:26 +0530

    [RA-9] Storybook - Used Link instead of anchor tag

--- src/app/components/Search/SearchSuggestionItem/SearchSuggestionItem.jsx
+++ src/app/components/Search/SearchSuggestionItem/SearchSuggestionItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './SearchSuggestionItem.scss';
 
 const SearchSuggestionItem = ({ suggestions }) => {
     const suggestionItem = suggestions.map((suggestion) => (
         <li key={suggestion.login} className="suggestion__item">
-            <a href={`/${suggestion.login}`}>{suggestion.login}</a>
+            <Link to={`/${suggestion.login}`}>{suggestion.login}</Link>
         </li>
     ));
 
